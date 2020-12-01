b29ccfd5155e09f637b8ecddedf955f4020af4e1
Soitora <simon.mattila@protonmail.com>
2020-12-01 13:53:25 +0100

    Initial set of files

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ui: {
+          background: 'var(--color-ui-background)',
+          sidebar: 'var(--color-ui-sidebar)',
+          typo: 'var(--color-ui-typo)',
+          primary: 'var(--color-ui-primary)',
+          border: 'var(--color-ui-border)'
+        }
+      },
+      spacing: {
+        sm: '24rem'
+      },
+      screens: {
+        xxl: '1400px'
+      },
+      fontWeight: {
+        black: 500,
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
