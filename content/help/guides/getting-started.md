2146b18e909115eb9391f716b827a159a13dfcd7
Andreas E <andreas.everos@gmail.com>
2020-12-15 09:49:04 +0100

    Add back VersionTag for getting started

--- content/help/guides/getting-started.md
+++ content/help/guides/getting-started.md
@@ -4,13 +4,14 @@ sidebar: default
 next: /help/guides/troubleshooting-problems/
 ---
 
+import VersionTag from '~/components/VersionTag.vue'
 
 # Getting started
 
 ## Installation
 
 You can download the latest version of **Tachiyomi** by going to our [download](/download/) page.
-When you've completed the download, open the `<VersionTag fileName/>` file and proceed with installing it.
+When you've completed the download, open the <VersionTag /> file and proceed with installing it.
 
 ## Installing an extension
 
---
+++ src/components/VersionTag.vue
@@ -0,0 +1,41 @@
+<template>
+	<span class="fileNameContainer" title="File name">
+		<MaterialIcon class="fileNameIcon" icon="get_app" />
+		<span class="fileName">tachiyomi-v{{ this.$data.tagName }}.apk</span>
+		<slot />
+	</span>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			tagName: "0.00.0",
+		};
+	},
+	async mounted() {
+        let stable = await this.$fetchers.tachiyomi().stable(this.$store)
+        this.tagName = stable.data.version
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.fileNameContainer {
+	white-space: nowrap;
+	.fileNameIcon {
+		color: var(--inline-code-text);
+		font-weight: 500;
+		font-size: 1em;
+		vertical-align: top;
+	}
+	.fileName {
+		color: var(--inline-code-text);
+		font-weight: 500;
+		font-size: 1em;
+	}
+	&:hover {
+		cursor: default;
+	}
+}
+</style>
