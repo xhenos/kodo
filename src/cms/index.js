ab2f53667a1d6fc617a7e9047811854d6077f2b1
Soitora <simon.mattila@protonmail.com>
2021-03-03 13:44:10 +0100

    Rename Description to Body in Container CMS
    Co-Authored-By: Andreas <6576096+ghostbear@users.noreply.github.com>

--- src/cms/index.js
+++ src/cms/index.js
@@ -24,7 +24,7 @@ CMS.registerEditorComponent({
 			],
 		},
 		{ name: "title", label: "Title", widget: "string", required: "false" },
-		{ name: "description", label: "Description", widget: "string", required: "true" },
+		{ name: "body", label: "Body", widget: "markdown", required: "true" },
 	],
 	pattern: /^\s?:::(guide|alert|c-tip|c-warning|c-danger|note|videolink|collapse)(.+)?\s+?(.+)\s+:::$/,
 	fromBlock: function(match) {
@@ -32,21 +32,21 @@ CMS.registerEditorComponent({
 			type: match[1],
 			title: match[2],
 			titleFallback: match[2] || "",
-			description: match[3],
+			body: match[3],
 		};
 	},
 	toBlock: function(obj) {
-		return `:::${obj.type} ${obj.title}\n${obj.description}\n:::`;
+		return `:::${obj.type} ${obj.title}\n${obj.body}\n:::`;
 	},
 	toPreview: function(obj) {
 		if (obj.type.startsWith("c-")) {
-			return `<div class="custom-block ${obj.type}"><p class="c-title">${obj.titleFallback}</p><p>${obj.description}</p></div>`;
+			return `<div class="custom-block ${obj.type}"><p class="c-title">${obj.titleFallback}</p><p>${obj.body}</p></div>`;
 		} else if (obj.type == "videolink") {
-			return `<div class="videolink"><p><a href="${obj.description}"></a></p></div>`;
+			return `<div class="videolink"><p><a href="${obj.body}"></a></p></div>`;
 		} else if (obj.type == "collapse") {
-			return `<details class="custom-block c-details"><summary class="c-title">${obj.titleFallback}</summary><p>${obj.description}</p></details>`;
+			return `<details class="custom-block c-details"><summary class="c-title">${obj.titleFallback}</summary><p>${obj.body}</p></details>`;
 		} else {
-			return `<div class="custom-block c-${obj.type}"><p class="c-title">${obj.titleFallback}</p><p>${obj.description}</p></div>`;
+			return `<div class="custom-block c-${obj.type}"><p class="c-title">${obj.titleFallback}</p><p>${obj.body}</p></div>`;
 		};
 	},
 });
