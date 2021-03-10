c08ed13855b0950ca4eb682d6f4e95ac7d1d770d
Soitora <simon.mattila@protonmail.com>
2021-03-10 17:07:25 +0100

    Fix errors caused by Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,42 +1,15 @@
 <template>
 	<ClientOnly>
-		<carousel :perPage="1">
-			<slide v-for="item in items" :key="item">
-				<img class="slide-image" v-bind:src="item.image" />
-				<p class="slide-title">
-					<b v-if="item.title">{{ item.title }}</b>
-				</p>
-				<p class="slide-desc" v-if="item.desc">{{ item.desc }}</p>
-			</slide>
-		</carousel>
+		<VueCarousel :items="items" />
 	</ClientOnly>
 </template>
 
 <script>
+import VueCarousel from "./VueCarousel";
 export default {
+	components: { VueCarousel },
 	props: {
 		items: Array,
 	},
-	components: {
-		Carousel: () =>
-			import("vue-carousel")
-				.then(m => m.Carousel)
-				.catch(),
-		Slide: () =>
-			import("vue-carousel")
-				.then(m => m.Slide)
-				.catch(),
-	},
 };
 </script>
-
-<style>
-.slide-image {
-	margin: auto;
-}
-
-.slide-title,
-.slide-desc {
-	text-align: center;
-}
-</style>
---
+++ src/components/VueCarousel.vue
@@ -0,0 +1,40 @@
+<template>
+	<carousel :perPage="1">
+		<slide v-for="item in items" :key="item.title">
+			<img class="slide-image" v-bind:src="item.image" />
+			<p class="slide-title">
+				<b v-if="item.title">{{ item.title }}</b>
+			</p>
+			<p class="slide-desc" v-if="item.desc">{{ item.desc }}</p>
+		</slide>
+	</carousel>
+</template>
+
+<script>
+export default {
+	props: {
+		items: Array,
+	},
+	components: {
+		Carousel: () =>
+			import("vue-carousel")
+				.then(m => m.Carousel)
+				.catch(),
+		Slide: () =>
+			import("vue-carousel")
+				.then(m => m.Slide)
+				.catch(),
+	},
+};
+</script>
+
+<style>
+.slide-image {
+	margin: auto;
+}
+
+.slide-title,
+.slide-desc {
+	text-align: center;
+}
+</style>
