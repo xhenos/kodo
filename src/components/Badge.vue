b91131ed64c2a5c1a620df60dcc9859096936054
Andreas E <andreas.everos@gmail.com>
2021-03-13 16:46:47 +0100

    Update style for Badge.vue

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,19 +1,12 @@
 <template>
-	<span
-		class="badge px-2 h-6 rounded-full text-xs font-semibold flex items-center"
-		:class="`bg-${color}-700 text-${color}-100`"
-	>
-		<material-icon v-if="icon" :icon="icon" color="color" class="mr-1" />
+	<span class="badge">
+		<material-icon v-if="icon" :icon="icon" class="mr-1" />
 		{{ text }}
 	</span>
 </template>
 <script>
 export default {
 	props: {
-		color: {
-			type: String,
-			default: "indigo",
-		},
 		icon: {
 			type: String,
 			default: "",
@@ -26,7 +19,13 @@ export default {
 </script>
 <style lang="stylus">
 .badge {
+	background-color var(--color-ui-primary)
+	border-radius 99em
 	display inline-flex
+	font-size 0.45em
+	margin 0 5px
+	padding 4px 16px
+	transform translateY(-0.5em)
 	width max-content
 
 	.material-holder {
