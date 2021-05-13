4b59b8582762223373848f996736534cee57cecc
Soitora <simon.mattila@protonmail.com>
2021-05-13 22:55:47 +0200

    Self-host Google Material Icons

--- src/components/MaterialIcon.vue
+++ src/components/MaterialIcon.vue
@@ -29,17 +29,37 @@ export default {
 </script>
 
 <style lang="stylus">
-@import "~material-design-icons/iconfont/material-icons.css"
+.material-icons {
+	font-family "Material Icons"
+	font-weight normal
+	font-style normal
+	font-size 1.35em
+	display inline-block
+	line-height 1
+	text-transform none
+	letter-spacing normal
+	word-wrap normal
+	white-space nowrap
+	direction ltr
+	position relative
+	top 0.2rem
+
+	// Support for all WebKit browsers.
+	-webkit-font-smoothing antialiased
+	// Support for Safari and Chrome.
+	text-rendering optimizeLegibility
+
+	// Support for Firefox.
+	-moz-osx-font-smoothing grayscale
+
+	// Support for IE.
+	font-feature-settings "liga"
+}
+
 
 .material-holder {
 	margin 0
 	font-size 0.85em
 	border-radius 3px
-}
-
-.material-icons {
-	font-size 1.35em
-	position relative
-	top 0.2rem
 }
 </style>
