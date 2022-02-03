e59d440bce5031b961318e4f58e0748c862e3b9c
Andreas <andreas.everos@gmail.com>
2022-02-03 13:24:14 +0100

    Add filter logic to Extension site and update README

--- README.md
+++ README.md
@@ -13,11 +13,17 @@ yarn install
 ...then start:
 
 ```bash
+# Run all 
 yarn dev
+# Run specific 
+# Replace <workspace-name> with tachiyomi-landing, tachiyomi-download, or tachiyomi-extensions
+yarn workspace <workspace-name> dev
 ```
 
 Navigate to [localhost:8080](http://localhost:8080) for landing page
+
 Navigate to [localhost:8081](http://localhost:8081) for download page
+
 Navigate to [localhost:8082](http://localhost:8082) for extensions page
 
 If you're using [Visual Studio Code](https://code.visualstudio.com/) we recommend installing the official extension [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode). If you are using other editors you may need to install a plugin in order to get syntax highlighting and intellisense.
--- packages/extensions/src/App.svelte
+++ packages/extensions/src/App.svelte
@@ -1,48 +1,31 @@
 <script>
 	import Layout from "tachiyomi-common/src/components/Layout.svelte";
 	import Extension from "./components/Extension.svelte";
-	import { Octokit } from "@octokit/core";
+	import API from "./scripts/api"
+
+	const api = new API()
 
 	let languages = []
-	let groups = {}
 
-	const octokit = new Octokit()
-	octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-		owner: 'tachiyomiorg',
-		repo: 'tachiyomi-extensions',
-		path: 'index.min.json',
-		ref: "repo"
-	})
-	.then(value => {
-		return JSON.parse(atob(value.data.content, 'base64').toString())
-	})
-	.then(value => {
-		return value.reduce((previousValue, currentValue) => {
-			(previousValue[currentValue['lang']] = previousValue[currentValue['lang']] || []).push(processEntry(currentValue))
-			return previousValue
-		}, {})
-	})
-	.then(value => {
-		languages = Object.keys(value),
-		groups = value
+	let query = ''
+	let selectedLanguages = []
+	let sortDirection = "asc"
+	let nsfwPreference = "meh"
+
+
+	api.getLanguages().then((value) => {
+		languages = value
 	})
 
-	function processEntry(value) {
-		return {
-          id: value.pkg.replace('eu.kanade.tachiyomi.extension.', ''),
-          pkg: value.pkg,
-          lang: value.lang,
-          code: value.code,
-          version: `v${value.version}`,
-          nsfw: value.nsfw === 1,
-          hasReadme: value.hasReadme === 1,
-          hasChangelog: value.hasChangelog === 1,
-          name: value.name.split(': ')[1],
-          iconUrl: `https://raw.githubusercontent.com/tachiyomiorg/tachiyomi-extensions/repo/icon/${value.apk.substring(0, value.apk.lastIndexOf('.'))}.png`,
-          downloadUrl: `https://raw.githubusercontent.com/tachiyomiorg/tachiyomi-extensions/repo/apk/${value.apk}`,
-          sources: value.sources
-        }
-	}
+	let extensions = {}
+
+	$: currentLanguages = selectedLanguages.length !== 0 ? selectedLanguages : languages
+	$: (async function() {
+		extensions = await api.getExtensions(query, selectedLanguages, nsfwPreference, sortDirection)
+	})()
+
+	$: console.log(extensions)
+
 
 	function openReadme(value) {
 		console.log("Open readme", value)
@@ -70,36 +53,43 @@
 	}
 
 	
+	
 </script>
 
 <Layout>
 	<h1>Extensions</h1>
-	<div><input type="text" /></div>
 	<div>
-		<select multiple>
-			{#each languages as language }
+		<input type="text" bind:value={query} />
+		<p>{query}</p>
+	</div>
+	<div>
+		<select bind:value={selectedLanguages} multiple>
+			{#each (languages || []) as language }
 			<option value={language}>{langName(language)}</option>
 			{/each}
 		</select>
+		<p>{selectedLanguages}</p>
 	</div>
 	<div>
-		<input type="radio" id="desc" name="sort" value="desc" checked>
+		<input type="radio" id="desc" name="sort" value="desc" bind:group={sortDirection}>
 		<label for="desc">Descending</label>
-		<input type="radio" id="asc" name="sort" value="asc">
+		<input type="radio" id="asc" name="sort" value="asc" bind:group={sortDirection}>
 		<label for="asc">Ascending</label>
+		<p>{sortDirection}</p>
 	</div>
 
 	<div>
-		<input type="radio" id="yes" name="nsfw" value="yes" checked>
+		<input type="radio" id="yes" name="nsfw" value="yes" bind:group={nsfwPreference}>
 		<label for="yes">Yes</label>
-		<input type="radio" id="no" name="nsfw" value="no">
+		<input type="radio" id="no" name="nsfw" value="no" bind:group={nsfwPreference}>
 		<label for="no">No</label>
-		<input type="radio" id="meh" name="nsfw" value="meh">
+		<input type="radio" id="meh" name="nsfw" value="meh" bind:group={nsfwPreference}>
 		<label for="meh">Don't care</label>
+		<p>{nsfwPreference}</p>
 	</div>
-	{#each languages as language }
+	{#each currentLanguages as language }
 		<h1>{langName(language)}</h1>
-		{#each groups[language] as extension }
+		{#each extensions[language] || [] as extension (extension.id) }
 			<Extension extension={extension} on:readme={openReadme(extension)} on:changelog={openChangelog(extension)} />
 		{/each}
 	{/each}
