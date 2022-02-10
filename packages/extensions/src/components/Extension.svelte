35dd1862efdd1155787ce8ed91ee99cfba789ee3
Soitora <simon.mattila@protonmail.com>
2022-02-10 20:25:51 +0100

    Make extension list more readable
    Adapts code from V2 into this V3 project, it is not meant to be completed, just to get it readable for now.

--- packages/extensions/src/components/Extension.svelte
+++ packages/extensions/src/components/Extension.svelte
@@ -1,9 +1,10 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import Icon from "tachiyomi-common/src/components/Icon.svelte";
 
   export let extension;
 
-  const { id, iconUrl, name, code, hasReadme, hasChangelog, downloadUrl } =
+  const { id, iconUrl, name, version, hasReadme, hasChangelog, downloadUrl } =
     extension;
 
   const dispatch = createEventDispatcher();
@@ -17,23 +18,106 @@
   }
 </script>
 
-<div {id}>
-  <img src={iconUrl} loading="lazy" alt={"Icon for " + name} />
-  <h2>{name} <span>{code}</span></h2>
-  <p>{id}</p>
-  <div>
-    {#if hasReadme}
-      <a href={`#${id}`} on:click={onReadme}>Help</a>
-    {/if}
-    {#if hasChangelog}
-      <a href={`#${id}`} on:click={onChangelog}>Changelog</a>
-    {/if}
-    <a href={downloadUrl}>Download</a>
+<div class="extension" {id}>
+  <img class="extension-icon" src={iconUrl} loading="lazy" alt={"Icon for " + name} />
+  <div class="extension-info">
+    <span class="extension-name">{name} <span class="extension-version">{version}</span></span>
+    <a class="extension-id" href={"#" + id}>{id}</a>
+  </div>
+  {#if hasChangelog || hasReadme}
+    <div class="extension-button-secondary">
+      {#if hasChangelog}
+        <a class="extension-changelog" href={`#${id}`} on:click={onChangelog}>Changelog</a>
+      {/if}
+      {#if hasReadme}
+        <a class="extension-readme" href={`#${id}`} on:click={onReadme}>Help</a>
+      {/if}
+    </div>
+  {/if}
+  <div class="extension-button-primary">
+    <a class="extension-download" href={downloadUrl}>
+      <Icon name="download" height="14" width="14" />
+      <span>Download</span>
+    </a>
   </div>
 </div>
 
 <style>
-  span {
-    background-color: tomato;
+  .extension {
+    align-items: center;
+    display: flex;
+    padding: 0.4em 1.5em;
+    max-width: 600px;
+    border-radius: 6px;
+    margin: 4px;
+  }
+  .extension:hover {
+    background-color: #f5f5f5;
+  }
+  .extension:target {
+    background-color: #ebebeb;
+  }
+  .extension-icon {
+    margin-right: .5em;
+    height: 3em;
+    width: 3em;
+  }
+  .extension-info {
+    flex: 1;
+  }
+  .extension-name {
+    font-weight: 600;
+    display: block;
+  }
+  .extension-id {
+    color: #6c757d;
+    font-family: monospace;
+    font-size: 0.9em;
+    text-decoration: none;
+  }
+  .extension-id:hover {
+    opacity: 0.8;
+  }
+  .extension-version {
+    background-color: #4f46e5;
+    color: #ffffff;
+    font-size: 0.75em;
+    font-weight: 500;
+    border-radius: 1em;
+    padding: 2px 8px;
+    margin: 0px 4px;
+  }
+  .extension-download {
+    color: #4f46e5;
+    fill: #4f46e5;
+    font-weight: 700;
+    border: 1px solid #e5e7eb;
+    border-radius: .5rem;
+    margin-right: .5em;
+    padding: .5rem 1rem;
+    transition-property: background-color, border-color, color, fill, stroke;
+    text-decoration: none;
+  }
+  .extension-download:hover {
+    color: #e5e7eb;
+    fill: #e5e7eb;
+    background-color: #4f46e5;
+    border: 1px solid #4f46e5;
+  }
+  .extension-button-secondary {
+    margin-right: 8px;
+  }
+  .extension-changelog,
+  .extension-readme {
+    display: block;
+    text-align: end;
+    font-size: 1rem;
+    font-weight: 600;
+    color: #4f46e5;
+    text-decoration: none;
+  }
+  .extension-changelog:hover,
+  .extension-readme:hover {
+    color: #af408e;
   }
 </style>
