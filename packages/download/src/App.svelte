a0f414f9b5682f34c887e6e9ab189c50631f1049
Andreas <andreas.everos@gmail.com>
2022-02-22 17:02:57 +0100

    Initial design for download page

--- packages/download/src/App.svelte
+++ packages/download/src/App.svelte
@@ -10,6 +10,8 @@
   let stable = undefined;
   let preview = undefined;
 
+  $: console.log("Stable", stable, "Preview", preview)
+
   $: stableUrls = stable?.downloads;
   $: previewUrls = preview?.downloads;
 
@@ -26,85 +28,39 @@
 </script>
 
 <Layout>
-  <div class="main">
-    <div class="branding">
-      <Icon name="logo" height="80" width="80" />
-      <h2>Download</h2>
-    </div>
-    <div class="description">
-      Download the latest stable version of Tachiyomi the preview version.
-    </div>
-    <div class="requirement">
-      {System.OSName == "iOS" || System.OSName == "MacOS"
-        ? "Unfortunately we only support Android, anyone claiming otherwise should not be trusted."
-        : ""}
-    </div>
-    <div
-      class="download {System.OSName == 'Android' ||
-      System.OSName == 'Windows' ||
-      System.OSName == 'Linux' ||
-      System.OSName == '(n/a)'
-        ? ''
-        : 'unsupported'}"
-    >
-      <a href={stableUrls?.universal}>
-        <button class="button large" id="stable"> Stable </button>
-      </a>
-      <a href={previewUrls?.universal}>
-        <button class="button large" id="preview"> Preview </button>
-      </a>
-    </div>
-    <div class="debug">
-      <h1>Stable</h1>
-      {JSON.stringify(stable)}
-      <h1>Preview</h1>
-      {JSON.stringify(preview)}
-    </div>
+  <div class="container">
+    <h1>Downloads</h1>
+    <p>The latest version is {stable?.version} of Tachiyomi. This is the most recent maintaned build. It was leleased {stable?.publishedAt}, on {stable?.publishedAt}</p>
+    <a href={stableUrls?.universal}>
+      <button class="button large inverted">
+        Download
+      </button>
+    </a>
+    <button class="button large">
+      Releast notes
+    </button>
+
+    <h2>Feeling adventurous?</h2>
+    <p>Try out the preview builds. To see the upcomming features. But do be warned that it can contain breaking changes and bugs.</p>
+    <p>The latest version is {preview?.version} of Tachiyomi. This is the most recent maintaned build. It was leleased {preview?.publishedAt}, on {preview?.publishedAt}</p>
+
+    <a href={previewUrls?.universal}>
+      <button class="button large inverted">
+        Download
+      </button>
+    </a>
+    <button class="button large">
+      Releast notes
+    </button>
   </div>
 </Layout>
 
 <style>
-  .main {
-    align-items: center;
-    justify-content: center;
-    margin: 8rem auto 2rem;
-    padding-right: 1rem;
-    padding-left: 1rem;
+  p {
+    max-width: 512px;
   }
 
-  .branding {
-    text-align: center;
-    align-items: center;
-    margin-bottom: 0.5rem;
-  }
-
-  .branding h2 {
-    margin-top: 1.25rem;
-    margin-block-end: 0.5rem;
-    border-style: none;
-    letter-spacing: -0.05em;
-    font-weight: 500;
-    font-size: 2.4rem;
-  }
-
-  .description {
-    text-align: center;
-    font-weight: 400;
-    font-size: 1.2rem;
-    margin-bottom: 0.75rem;
-  }
-
-  .requirement {
-    text-align: center;
-    font-weight: 500;
-    font-size: 1rem;
-  }
-
-  .download {
-    text-align: center;
-  }
-
-  .download.unsupported {
-    display: none;
+  h2 {
+    margin-top: 5rem;
   }
 </style>
