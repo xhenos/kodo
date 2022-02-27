171344c40c8b5a583bef172ee3470abff6a14c56
Soitora <simon.mattila@protonmail.com>
2022-02-27 19:15:28 +0100

    Stylize parts of the Download page
    Adds warning
    Fixed spelling
    Adds styling
    Adds relative time with actual time hover (no mobile support)

--- packages/download/src/App.svelte
+++ packages/download/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Layout from "@tachiyomi/shared/src/components/Layout.svelte";
-  import Icon from "@tachiyomi/shared/src/components/Icon.svelte";
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime";
   import API from "./data";
   import System from "svelte-system-info";
   import { onMount } from "svelte";
@@ -21,6 +22,13 @@
     preview = previewResult;
   });
 
+  dayjs.extend(relativeTime)
+
+  $: stablePublish = dayjs(stable?.publishedAt).format("dddd, MMMM D, YYYY HH:mm")
+  $: stablePublishRelative = dayjs(stable?.publishedAt).fromNow()
+  $: previewPublish = dayjs(preview?.publishedAt).format("dddd, MMMM D, YYYY HH:mm")
+  $: previewPublishRelative = dayjs(preview?.publishedAt).fromNow()
+
   console.log("BrowserName", System.BrowserName);
   console.log("BrowserVersion", System.BrowserVersion);
   console.log("OSName", System.OSName);
@@ -30,19 +38,25 @@
 <Layout>
   <div class="container">
     <h1>Downloads</h1>
-    <p>The latest version is {stable?.version} of Tachiyomi. This is the most recent maintaned build. It was leleased {stable?.publishedAt}, on {stable?.publishedAt}</p>
+    <p><strong class="brand">Tachiyomi</strong> is only safely downloaded from this website and our <a href="https://github.com/tachiyomiorg/tachiyomi" target="_blank">GitHub repository</a>.</p>
+    <p>You need <strong>Android 6.0</strong> or higher to use the app, we <em>do not</em> support <strong>iOS</strong>.</p>
+    <p>
+      The latest stable version of <strong class="brand">Tachiyomi</strong> is <strong>{stable?.version}</strong> and was released <strong title="{stablePublish}">{stablePublishRelative}</strong>.
+    </p>
     <a href={stableUrls?.universal}>
       <button class="button large inverted">
         Download
       </button>
     </a>
     <button class="button large">
-      Releast notes
+      Release notes
     </button>
 
     <h2>Feeling adventurous?</h2>
-    <p>Try out the preview builds. To see the upcomming features. But do be warned that it can contain breaking changes and bugs.</p>
-    <p>The latest version is {preview?.version} of Tachiyomi. This is the most recent maintaned build. It was leleased {preview?.publishedAt}, on {preview?.publishedAt}</p>
+    <p>Try out the preview builds to see the upcoming features, but be warned that it can contain breaking changes and bugs.</p>
+    <p>
+      The most recently maintained version of <strong class="brand">Tachiyomi</strong> is <strong>{preview?.version}</strong> and was released <strong title="{previewPublish}">{previewPublishRelative}</strong>.
+    </p>
 
     <a href={previewUrls?.universal}>
       <button class="button large inverted">
@@ -50,7 +64,7 @@
       </button>
     </a>
     <button class="button large">
-      Releast notes
+      Release notes
     </button>
   </div>
 </Layout>
@@ -62,5 +76,9 @@
 
   h2 {
     margin-top: 5rem;
+  }
+
+  .brand {
+    font-weight: 500;
   }
 </style>
