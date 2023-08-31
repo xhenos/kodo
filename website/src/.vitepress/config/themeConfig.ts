9f3fdfbef48e5c38d75de58450bba2ad3edc5444
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-08-31 20:26:36 -0300

    Rollback X change in favour of later Discord poll.

--- website/src/.vitepress/config/themeConfig.ts
+++ website/src/.vitepress/config/themeConfig.ts
@@ -27,11 +27,9 @@ const themeConfig: DefaultTheme.Config = {
 			ariaLabel: "Discord Server",
 		},
 		{
-			icon: {
-				svg: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>',
-			},
+			icon: "twitter",
 			link: "https://twitter.com/tachiyomiorg",
-			ariaLabel: "X (ex-Twitter) Page",
+			ariaLabel: "Twitter Page",
 		},
 		{
 			icon: "facebook",
@@ -44,7 +42,7 @@ const themeConfig: DefaultTheme.Config = {
 			},
 			link: "https://reddit.com/r/Tachiyomi",
 			ariaLabel: "Support subreddit",
-		  },
+		},
 		//{ icon: "instagram", link: "https://instagram.com/tachiyomiorg", ariaLabel: "Instagram Page" },
 	],
 
