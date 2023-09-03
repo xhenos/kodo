c5d61033b09d725484557842b5ccde77b24aaafb
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-09-03 20:39:25 -0300

    Show release contributors.

--- website/src/.vitepress/theme/components/Changelogs.vue
+++ website/src/.vitepress/theme/components/Changelogs.vue
@@ -19,6 +19,29 @@ function renderMarkdown(string: string | null | undefined) {
 const dateFormatter = new Intl.DateTimeFormat("en", {
 	dateStyle: "medium",
 });
+
+function contributors(body: string) {
+	const list = [...body.matchAll(/(?<=\(|(, ))@(.*?)(?=\)|(, ))/g)]
+		.map((match) => match[2]);
+
+	return [...new Set(list)];
+}
+
+// @ts-expect-error
+const listFormatter = new Intl.ListFormat("en", {
+	style: "long",
+	type: "conjunction",
+});
+
+function contributorsText(body: string) {
+	const list = contributors(body);
+
+	if (list.length <= 3) {
+		return listFormatter.format(list);
+	}
+
+	return listFormatter.format([...list.slice(0, 2), `${list.length - 2} other contributors`]);
+}
 </script>
 
 <template>
@@ -44,6 +67,31 @@ const dateFormatter = new Intl.DateTimeFormat("en", {
 			{{ dateFormatter.format(new Date(release.published_at!)) }}
 		</time>
 		<div v-html="renderMarkdown(release.body)"></div>
+		<div v-if="contributors(release.body!).length > 0" class="contributors">
+			<h3>Contributors</h3>
+			<ul>
+				<li
+					v-for="contributor of contributors(release.body!)"
+					:key="contributor"
+				>
+					<a
+						:href="`https://github.com/${contributor}`"
+						target="_blank"
+						:title="`${contributor} profile on GitHub`"
+						:aria-label="`${contributor} profile on GitHub`"
+					>
+						<img
+							:src="`https://github.com/${contributor}.png?size=32`"
+							loading="lazy"
+							class="avatar"
+						>
+					</a>
+				</li>
+			</ul>
+			<div class="names">
+				{{ contributorsText(release.body!) }}
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -58,5 +106,32 @@ h2 {
 time {
 	font-size: 0.875rem;
 	color: var(--vp-c-text-2);
+}
+
+.contributors {
+	ul {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+		list-style-type: none;
+		padding-left: 0;
+
+		li + li {
+			margin-top: 0;
+		}
+	}
+
+	.avatar {
+		width: 32px;
+		height: 32px;
+		border-radius: 50%;
+		box-shadow: var(--vp-shadow-1);
+		border: 1px solid var(--vp-c-divider);
+	}
+
+	.names {
+		font-size: 0.875rem;
+		color: var(--vp-c-text-2);
+	}
 }
 </style>
