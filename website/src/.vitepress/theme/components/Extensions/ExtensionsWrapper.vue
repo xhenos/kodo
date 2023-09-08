4c01fa7ff2400ee9671213742bada1b5daf755a9
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-09-08 20:04:33 -0300

    Wrap the extensions wrapper in a client-only.

--- website/src/.vitepress/theme/components/Extensions/ExtensionsWrapper.vue
+++ website/src/.vitepress/theme/components/Extensions/ExtensionsWrapper.vue
@@ -106,16 +106,18 @@ watch([isLoading, loadingInstance], async ([newIsLoading]) => {
 </script>
 
 <template>
-	<ExtensionFilters
-		v-model:search="filters.search"
-		v-model:lang="filters.lang"
-		v-model:nsfw="filters.nsfw"
-		v-model:sort="filters.sort"
-		:extensions="extensions ?? []"
-	/>
-	<div class="extensions">
-		<ExtensionList v-if="!isLoading" :extensions="filteredExtensions" />
-	</div>
+	<ClientOnly>
+		<ExtensionFilters
+			v-model:search="filters.search"
+			v-model:lang="filters.lang"
+			v-model:nsfw="filters.nsfw"
+			v-model:sort="filters.sort"
+			:extensions="extensions ?? []"
+		/>
+		<div class="extensions">
+			<ExtensionList v-if="!isLoading" :extensions="filteredExtensions" />
+		</div>
+	</ClientOnly>
 </template>
 
 <style lang="stylus" scoped>
